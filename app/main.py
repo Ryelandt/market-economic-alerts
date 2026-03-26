70a4b6b971e2da7dc5261f832c4ac166165da5b9
ryelandt yohann <31223949+Ryelandt@users.noreply.github.com>
2026-03-26 11:51:11 +0100

    Update main.py
    add code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
+from app.sources.mock_calendar import fetch_events
+from app.core.decision_engine import score_event, classify_event
+from app.alerts.console_alert import send_alert
 
+def main():
+    events = fetch_events()
+
+    for event in events:
+        score = score_event(event)
+        level = classify_event(score)
+
+        if level != "IGNORE":
+            send_alert(event, level)
+
+if __name__ == "__main__":
+    main()
