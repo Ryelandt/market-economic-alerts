76aa5645eb4730739b34a515622f82e4e7b16740
ryelandt yohann <31223949+Ryelandt@users.noreply.github.com>
2026-03-26 15:07:42 +0100

    Update console_alert.py

--- app/alerts/console_alert.py
+++ app/alerts/console_alert.py
@@ -4,7 +4,10 @@ import requests
 def send_alert(event, level):
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
-
+    
+    print("BOT TOKEN =", os.getenv("TELEGRAM_BOT_TOKEN"))
+    print("CHAT ID   =", os.getenv("TELEGRAM_CHAT_ID"))
+    
     if not bot_token or not chat_id:
         raise RuntimeError("Telegram credentials not set")
 
