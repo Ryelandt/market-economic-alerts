37475884db58d66aad8a3e819cac704b7b7982df
ryelandt yohann <31223949+Ryelandt@users.noreply.github.com>
2026-03-27 13:51:45 +0100

    Update mock_calendar.py

--- app/sources/mock_calendar.py
+++ app/sources/mock_calendar.py
@@ -4,6 +4,7 @@ from app.models.event import EconomicEvent
 
 DATA_FILE = "app/data/economic_calendar.json"
 
+
 def fetch_events():
     events = []
 
@@ -11,16 +12,16 @@ def fetch_events():
         raw_events = json.load(f)
 
     for e in raw_events:
-          events.append(
-                    EconomicEvent(
-                        name=e["name"],
-                        datetime=datetime.fromisoformat(e["datetime"]),
-                        country=e["country"],
-                        importance=e["importance"],
-                        forecast=e.get("forecast"),
-                        actual=e.get("actual"),
-                        affected_assets=e["affected_assets"]
-                    )
-                )
+        events.append(
+            EconomicEvent(
+                name=e["name"],
+                datetime=datetime.fromisoformat(e["datetime"]),
+                country=e["country"],
+                importance=e["importance"],
+                forecast=e.get("forecast"),
+                actual=e.get("actual"),
+                affected_assets=e["affected_assets"]
+            )
+        )
 
     return events
