6cf27562a727987a64c63b9ece881829329b74bf
ryelandt yohann <31223949+Ryelandt@users.noreply.github.com>
2026-04-12 16:28:14 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import os
 
 from app.core.decision_engine import DecisionEngine
 from app.alerts.telegram_alert import TelegramAlert
+from app.models.event import Even
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("macro-alerts")
