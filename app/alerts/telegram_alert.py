e286c20b15353330c1044ba8c0c53677a77a975c
ryelandt yohann <31223949+Ryelandt@users.noreply.github.com>
2026-04-12 17:40:34 +0200

    Update telegram_alert.py

--- app/alerts/telegram_alert.py
+++ app/alerts/telegram_alert.py
@@ -28,16 +28,7 @@ class TelegramAlert:
         payload = {
             "chat_id": self.chat_id,
             "text": message,
-            "reply_markup": {
-                "inline_keyboard": [
-                    [
-                        {
-                            "text": "🔁 Relancer le batch maintenant",
-                            "callback_data": "RERUN_BATCH"
-                        }
-                    ]
-                ]
-            }
+           
 
         }
 
